3f736b5bce0af8e9b1152d0dd40f88c4f919f510
Mantas Mozuraitis <mozmantas@gmail.com>
2023-10-08 00:10:45 +0100

    implemented form submittion

--- scripts.js
+++ scripts.js
@@ -1,15 +1,23 @@
-var cards = document.querySelectorAll(".card");
-
-cards.forEach((card) => {
+document.querySelectorAll(".card").forEach((card) => {
     card.addEventListener("click", ()=>{
         displayForm(card.getAttribute("id"));
     })
 })
+
+document.querySelector("#orderForm").addEventListener("submit", ()=>{
+    alert("Form has been submitted");
+});
+
 function toggleForm(){
     document.querySelector("fieldset").classList.toggle("hidden");
     document.querySelector("form").classList.toggle("hidden");
 }
 
+function selectPlan(cardID){
+    document.querySelector("#plan_name").value = cardID;
+}
+
 function displayForm(cardID){
     toggleForm();
+    selectPlan(cardID);
 }
