2c35c6b77b1f32bc312cbb40ba2942135c6dc4e8
Mantas Mozuraitis <mozmantas@gmail.com>
2023-10-10 11:51:18 +0100

    fixed the form-to-email.php to send emails

--- form-to-email.php
+++ form-to-email.php
@@ -1,25 +1,18 @@
 <?php
-if(!isset($_POST['submit'])){
-    echo "error; please submit the form"
-}
-$name = $_POST['user_name'];
-$email = $_POST['user_email'];
-$message = $_POST['user_message'];
+   ini_set( 'display_errors', 1 );
+   error_reporting( E_ALL );
 
-if(empty($name) || empty($email)){
-    echo "Missing name or email";
-    exit;
-}
+   $from = $_POST['user_email'];
+   $to = "getonline@mozuweb.com";
+   $subject = $_POST['plan_name'] . " order request";
+   $message = $_POST['user_message'];
 
-$email_from = "mozuwebdevelopment@gmail.com";
-$email_subject = "New order request";
-$email_body = "New Message \n",
-        "from: $email \n",
-        "Message: $message \n"
-$to = "mozuwebdevelopment@gmail.com";
-$headers = "From: $email_from \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-header('Location: ./index.html');
-exit;
+   $headers = "MIME-Version: 1.0" . "\r\n";
+   $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+   $headers = "From:" . $from;
+   if(mail($to,$subject,$message, $headers)) {
+      echo '<script>alert("Message was sent successfully")</script>'; 
+   } else {
+      echo '<script>alert("Message was not sent")</script>'; 
+   }
 ?>
